a1c6b933dea22215b64b1234eeada78ba52a686a
Pat Ransil <pat.ransil@gmail.com>
2023-12-21 17:08:06 -0800

    Create AddCoordToGPXFile.sh

---
+++ AddCoordToGPXFile.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+# Open file.gpx, add a coordinate point (lat, lon, time), close the file
+echo
+    <rtept lat="-20.531781" lon="-152.567304">
+      <name>This is point 11</name>
+      <ele>0</ele>
+      <time>2023-01-01T00:09:00Z</time>
+    </rtept>
